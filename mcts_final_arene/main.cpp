cccae9b8fdc05a22c64561bcdf5d07384795e953
Youssef <90571117+YoussefNSI@users.noreply.github.com>
2025-03-17 15:39:29 +0100

    .

---
+++ mcts_final_arene/main.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <mutex>
+
+#include "arbitre.h"
+#include "MCTS.h"
+#include "Trainer.h"
+
+#define NB_PARTIES 50
+using namespace std;
+
+int main()
+{
+    MCTSTrainer trainer(3000, 500);
+    trainer.train();
+    std::cout << "Entraînement terminé" << std::endl;
+
+    //initialise la graine du générateur aléatoire
+    std::srand(std::time(nullptr));
+
+    // création de l'Arbitre (graine , joueur 1, joueur 2 , nombre de parties)
+    Arbitre a (player::RAND, player::MCTS, NB_PARTIES);
+    // commence le challenge
+    a.challenge();
+    return 0;
+}
